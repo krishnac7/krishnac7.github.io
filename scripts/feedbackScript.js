85a62d407ff577c66349d4e835e7dcdee9b2bd0b
krishnac7 <kckckrishnachaitanya7@gmail.com>
2019-01-08 14:58:51 +0530

    updated feedback

---
+++ scripts/feedbackScript.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){ 
+      $('#submit').click(function(e){
+          e.preventDefault();
+          console.log("clicked");
+          var rating = $('form').serializeArray()[0].value;
+          var feedback = $('form').serializeArray()[1].value;
+          const Url ="https://docs.google.com/forms/d/e/1FAIpQLSdQ_rLouDZ3wmjiAMg9npEMEGpN7MBygeFTpFr0YlE7QzUF7g/formResponse?usp=pp_url&entry.1343283815="+rating+"&entry.27308448="+encodeURIComponent(feedback)+"&submit=Submit"
+          $.ajax({
+              url:Url,
+              type:"POST"
+             
+          })
+          
+      });
+  });
